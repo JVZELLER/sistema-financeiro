8d550e0475bd2e5296aff838ac6acff0c8035854
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-19 13:39:54 -0300

    docs: add docs to 'SistemaFinanceiro' module

--- lib/sistema_financeiro.ex
+++ lib/sistema_financeiro.ex
@@ -1,4 +1,16 @@
 defmodule SistemaFinanceiro do
+  @moduledoc """
+  Core module of the app where you can start the application
+  and play around with the Accounts
+  """
+
+  @doc """
+  Starts the app
+
+  ## Examples
+    iex> SistemaFinanceiro.start()
+    :start
+  """
   def start do
     :start
   end
