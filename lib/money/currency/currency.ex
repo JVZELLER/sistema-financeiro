ac8941cf7526f4751699a2e3e8317d2a611d2d17
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-19 17:28:22 -0300

    docs: add 'docs' to 'Currency' module

--- lib/money/currency/currency.ex
+++ lib/money/currency/currency.ex
@@ -1,6 +1,6 @@
 defmodule Currency do
   @moduledoc """
-  Estrutura de dados que representa moedas, seguindo os padrões da ISO 4217
+  Represents a Currency type accordingly to ISO 4217
   """
 
   alias __MODULE__, as: Currency
@@ -14,6 +14,20 @@ defmodule Currency do
             name: "Brazilian Real",
             symbol: "R$"
 
+  @doc """
+  Finds `Currency` from a given `alpha_code`
+
+  ## Examples:
+  ```
+    iex> Currency.find!(:BRL)
+    {:ok, Currency{alpha_code: "BRL", exponent: 2, name: "Brazilian Real", numeric_code: 986, symbol: "R$"}
+    iex> Currency.find!(:usd)
+    {:ok, Currency{alpha_code: "USD", exponent: 2, name: "US Dollar", numeric_code: 840, symbol: "$"}
+    iex> Currency.find!("brl")
+    {:ok, Currency{alpha_code: "BRL", exponent: 2, name: "Brazilian Real", numeric_code: 986, symbol: "R$"}
+    iex> Currency.find!(86)
+    ** (ArgumentError) "86" must be atom or string
+  """
   def find!(alpha_code) do
     cond do
       is_atom(alpha_code) ->
@@ -27,10 +41,20 @@ defmodule Currency do
         |> String.to_atom()
         |> get!()
 
-      true -> raise(ArgumentError, message: "#{alpha_code} must be atom or string")
+      true -> raise(ArgumentError, message: "\"#{alpha_code}\" must be atom or string")
     end
   end
 
+  @doc """
+  Returns `Currency` exponent
+
+  ## Examples:
+  ```
+    iex> Currency.get_factor(Currency.find!(:BRL))
+    200
+    iex> Currency.get_factor(Currency.find!(:JPY))
+    0
+  """
   def get_factor(%Currency{exponent: exponent}) do
     :math.pow(10, exponent) |> round()
   end
@@ -40,12 +64,26 @@ defmodule Currency do
 
     case Map.fetch(currencies, alpha_code) do
       {:ok, currency} -> {:ok, currency}
-      :error -> raise ArgumentError,
-        message: "Currency #{alpha_code} not found"
+      :error -> {:error, raise_not_found_currency(alpha_code)}
     end
   end
 
+  @doc """
+  Returns the `alpha_code` represented as an `atom`
+
+  ## Examples:
+  ```
+    iex> Currency.to_atom(Currency.find!(:JPY)
+    :JPY
+    iex> Currency.to_atom(Currency.find!(:BRL)
+    :BRL
+  """
   def to_atom(%Currency{alpha_code: alpha_code}) do
     alpha_code |> String.to_existing_atom()
+  end
+
+  defp raise_not_found_currency(alpha_code) do
+    raise ArgumentError,
+      message: "Currency #{alpha_code} not found"
   end
 end
