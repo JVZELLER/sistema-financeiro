83d0201db17b3839f3775655e1892cf7903f1224
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-19 13:36:17 -0300

    docs: add docs to 'Account' module

--- lib/account/account.ex
+++ lib/account/account.ex
@@ -1,8 +1,17 @@
 defmodule Account do
-  @moduledoc false
+  @moduledoc """
+  Defines an Account type
+  """
 
   defstruct [:code, :owner, :balance]
 
+  @doc """
+  Create a new Account type.
+
+  ## Examples
+    iex> Account.new("123", "Ze Doe", 100, :BRL)
+    %{code: "123", owner: "Ze Doe", balance: %Money{amount: 10000, currency: :BRL}}
+  """
   def new(code, owner, balance, currency_code) do
     %{
       code: code,
