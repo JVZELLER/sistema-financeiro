40a613d5dd43c192001746a09b09dbb92ce23287
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-19 21:32:44 -0300

    test: add 'Account.new/4' test

--- test/account/account_test.exs
+++ test/account/account_test.exs
@@ -1,4 +1,10 @@
 defmodule AccountTest do
   use ExUnit.Case
   doctest Account
+
+  test "Account.new/4" do
+    balance = Money.new(10)
+
+    assert %Account{code: "123", owner: "Doe", balance: balance} === Account.new("123", "Doe", 10, :BRL)
+  end
 end
