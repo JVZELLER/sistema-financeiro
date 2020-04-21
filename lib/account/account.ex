462d9cac978d129242da60e5b6bfcb26b81144cf
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-20 22:28:42 -0300

    feature: add 'Account.update/1'

--- lib/account/account.ex
+++ lib/account/account.ex
@@ -19,4 +19,19 @@ defmodule Account do
       balance: Money.new(balance, currency_code)
     }
   end
+
+  @doc """
+  Update an account
+
+  ## Examples:
+  ```
+    iex> account = Account.new("123", "Ze Doe", 100, :BRL)
+    iex> Account.update(account, %{owner: "Zé Doe"})
+    %Account{code: "123", owner: "Zé Doe", balance: %Money{amount: 10000, currency: :BRL}}
+    iex> Account.update(account, %{code: "321", owner: "Zeller"})
+    %Account{code: "321", owner: "Zeller", balance: %Money{amount: 10000, currency: :BRL}}
+  """
+  def update(account, to_update) do
+    account |> struct(to_update)
+  end
 end
