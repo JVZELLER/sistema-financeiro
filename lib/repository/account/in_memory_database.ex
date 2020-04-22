8f8918620db4aeff02f3a5d1e41bf19e6d9e87dc
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-22 01:32:40 -0300

    chore: change 'Account.InMemoryDatabase' data just to use more 'friendly' numbers and currencies

--- lib/repository/account/in_memory_database.ex
+++ lib/repository/account/in_memory_database.ex
@@ -12,9 +12,9 @@ defmodule Repository.Account.InMemoryDatabase do
       Account.new("2", "Zeller Doe", 50, :BRL),
       Account.new("3", "Ann Doe", 25, :USD),
       Account.new("4", "Smith Doe", 100, :USD),
-      Account.new("5", "Xang Doe", 4550, :JPY),
+      Account.new("5", "Xang Doe", 25, :USD),
       Account.new("6", "Zang Doe", 50, :JPY),
-      Account.new("7", "Xan Jack", 70, :JPY)
+      Account.new("7", "Xan Jack", 25, :JPY)
     ]
   end
 end
