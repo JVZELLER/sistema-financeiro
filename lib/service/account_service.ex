1ece0e3b31a0b8a43b5aa152577d0ee75cc49d68
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-22 01:34:57 -0300

    feature: add 'AccountService.exchange_money/2'

--- lib/service/account_service.ex
+++ lib/service/account_service.ex
@@ -2,7 +2,9 @@ defmodule SistemaFinanceiro.AccountService do
   @moduledoc """
   A lightfull service to orchestrate incoming calls/requests actions from our controller
   """
+
   alias Repository.Account.AccountRepository
+  alias Repository.ExchangeRateRepository
 
   @doc """
   Lists all accounts
@@ -17,9 +19,9 @@ defmodule SistemaFinanceiro.AccountService do
         Account.new("2", "Zeller Doe", 50, :BRL),
         Account.new("3", "Ann Doe", 25, :USD),
         Account.new("4", "Smith Doe", 100, :USD),
-        Account.new("5", "Xang Doe", 4550, :JPY),
+        Account.new("5", "Xang Doe", 25, :USD),
         Account.new("6", "Zang Doe", 50, :JPY),
-        Account.new("7", "Xan Jack", 70, :JPY)
+        Account.new("7", "Xan Jack", 25, :JPY)
       ]
     }
   """
@@ -36,7 +38,7 @@ defmodule SistemaFinanceiro.AccountService do
   ## Examples:
   ```
     iex> SistemaFinanceiro.AccountService.split_money(["1", "5"], "0.05", [3,7])
-    {:error, "Monies with different currencies. Got JPY and BRL"}
+    {:error, "Monies with different currencies. Got USD and BRL"}
     iex> SistemaFinanceiro.AccountService.split_money(["1", "2"], "0.05", [3,7])
     {:ok,
       [
@@ -50,10 +52,17 @@ defmodule SistemaFinanceiro.AccountService do
          code: "2",
          owner: "Zeller Doe"
        }
-      ]}
+      ]
+    }
   """
   def split_money(accounts_code, amount, ratios) do
-    {:ok, find_accounts_by_code(accounts_code) |> do_split(amount, ratios)}
+    {:ok, find_accounts_by_code(accounts_code) |> do_split!(amount, ratios)}
+  rescue
+    e -> {:error, e.message}
+  end
+
+  def exchange_money(accounts_code, to_currency_code) do
+    {:ok, find_accounts_by_code(accounts_code) |> do_exchange!(to_currency_code)}
   rescue
     e -> {:error, e.message}
   end
@@ -67,11 +76,14 @@ defmodule SistemaFinanceiro.AccountService do
 
         {:ok, account} ->
           account
+
+        {:error, reason} ->
+          raise ArgumentError, message: reason
       end
     end)
   end
 
-  defp do_split(accounts, amount, ratios) do
+  defp do_split!(accounts, amount, ratios) do
     money = Money.parse(amount)
     monies = Money.divide(money, ratios)
     allocate_money_to_accounts(accounts, monies)
@@ -87,6 +99,43 @@ defmodule SistemaFinanceiro.AccountService do
       ]
     else
       [Account.update(head_acc, %{balance: new_balance})]
+    end
+  end
+
+  defp do_exchange!(accounts, to_convert_currency_code) do
+    Enum.map(accounts, fn acc ->
+      m = acc.balance
+      from_currency = Currency.find!(m.currency)
+      to_currency = Currency.find!(to_convert_currency_code)
+
+      result =
+        same_currency(to_currency.alpha_code, from_currency.alpha_code) ||
+          ExchangeRateRepository.find(%{
+            to: to_currency.alpha_code,
+            from: from_currency.alpha_code
+          })
+
+      case result do
+        {:ok, nil} ->
+          raise ArgumentError,
+            message:
+              "Exchange Rate from #{from_currency.alpha_code} to #{to_currency.alpha_code} not found"
+
+        {:ok, exchange_rate} ->
+          Account.update(acc, %{
+            balance: Money.multiply(m, exchange_rate.rate, to_currency.alpha_code)
+          })
+
+        {:error, reason} ->
+          raise ArgumentError, message: reason
+      end
+    end)
+  end
+
+  defp same_currency(to, from) do
+    cond do
+      to === from -> {:ok, %{rate: 1}}
+      true -> false
     end
   end
 end
