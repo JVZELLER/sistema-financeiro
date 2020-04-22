cd33ef6c0ab0377123c6c08876577944aa5108df
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-22 01:30:04 -0300

    chore: add 'Repository.Currency.InMemoryDatabase' to configuration to ease the tests proccess

--- config/dev.exs
+++ config/dev.exs
@@ -2,4 +2,5 @@ import Config
 
 config :sistema_financeiro,
   account_database: Repository.Account.InMemoryDatabase,
-  currency_database: Repository.Currency.InMemoryDatabase
+  currency_database: Repository.Currency.InMemoryDatabase,
+  exchange_database: Repository.ExchangeRate.InMemoryDatabase
