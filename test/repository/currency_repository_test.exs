9bc563c7aed28ddf073ae27a545d7a64a311e694
Jose Victor Zeller Gonçalves <josevictorzg@gmail.com>
2020-04-22 02:15:18 -0300

    tests: add 'doctests' to repositories

---
+++ test/repository/currency_repository_test.exs
@@ -0,0 +1,4 @@
+defmodule CurrencyRepositoryTest do
+  use ExUnit.Case, async: true
+  doctest Repository.CurrencyRepository
+end
---
+++ test/repository/exchange_rate_repository_test.exs
@@ -0,0 +1,4 @@
+defmodule ExchangeRateRepositoryTest do
+  use ExUnit.Case, async: true
+  doctest Repository.ExchangeRateRepository
+end
